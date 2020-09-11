52678f7f46ed14ee652db4bdf840dc455dbac089
mostafa-just <71152481+mostafa-just@users.noreply.github.com>
2020-09-12 00:20:14 +0400

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,7 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> test_result = 70
+>>> if test_result >= 50
+SyntaxError: invalid syntax
+>>> if test_result >= 50:
+	Print(f("Test result: {test_result}. Pass"))
